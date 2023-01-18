84eb144a268749078c9d47200d69664d11fc2cb5
Toph Bantoto <lcbantoto@gmail.com>
2023-01-18 02:12:26 -0500

    9988899

--- src/constants/settings.ts
+++ src/constants/settings.ts
@@ -1,7 +1,10 @@
 import { solution } from '../lib/words'
 
 export const MAX_WORD_LENGTH = solution.length
-export const MAX_CHALLENGES = Math.max(0, Math.abs(MAX_WORD_LENGTH - 6) - 2) + 8
+export const MAX_CHALLENGES = Math.min(
+  Math.max(0, Math.abs(MAX_WORD_LENGTH - 6) - 1) + 8,
+  9
+)
 export const ALERT_TIME_MS = 2000
 export const REVEAL_TIME_MS = 350
 export const GAME_LOST_INFO_DELAY = (MAX_WORD_LENGTH + 1) * REVEAL_TIME_MS
@@ -13,9 +16,9 @@ export const BLOWFISH_KEY = 'jIEXqS7f%bYm#JiutG9Eu@2$mnjTpu'
 export const BLOWFISH_IV = 'sSd9EI@o'
 
 // 3, 2 -> 9
-// 4, 1 -> 8
-// 5, 0 -> 7
-// 6, 0 -> 7
-// 7, 0 -> 7
-// 8, 1 -> 8
+// 4, 1 -> 9
+// 5, 0 -> 8
+// 6, 0 -> 8
+// 7, 0 -> 8
+// 8, 1 -> 9
 // 9, 2 -> 9
