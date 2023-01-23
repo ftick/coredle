3233acf95ae0215806b453d6333a6f1c83453824
Toph Bantoto <lcbantoto@gmail.com>
2023-01-23 02:05:16 -0500

    word -> term

--- src/constants/strings-fr.ts
+++ src/constants/strings-fr.ts
@@ -3,7 +3,7 @@ export const GAME_TITLE = process.env.REACT_APP_GAME_NAME!
 export const WIN_MESSAGES = ['Bon travail!', 'Pas mal!', 'Formidable!']
 export const GAME_COPIED_MESSAGE = 'Jeu copié dans le presse-papiers'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Pas assez de lettres'
-export const WORD_NOT_FOUND_MESSAGE = 'Mot introuvable'
+export const WORD_NOT_FOUND_MESSAGE = 'Terme introuvable'
 export const HARD_MODE_ALERT_MESSAGE =
   "Le Mode Difficile ne peut être activé qu'au début!"
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
@@ -24,8 +24,8 @@ export const ENTER_TEXT = 'Entrée'
 export const DELETE_TEXT = 'Supprimé'
 export const STATISTICS_TITLE = 'Statistiques'
 export const GUESS_DISTRIBUTION_TEXT = 'Distribution des essais'
-export const NEW_WORD_TEXT = 'Nouveau mot en'
-export const NEXT_WORD_TEXT = 'Prochain Mot'
+export const NEW_WORD_TEXT = 'Nouvelle terme en'
+export const NEXT_WORD_TEXT = 'Prochaine Terme'
 export const SHARE_TEXT = 'Partager'
 export const WITH_LINK_TEXT = 'Avec Lien'
 export const TOTAL_TRIES_TEXT = "Nombre total d'essais"
@@ -42,13 +42,13 @@ export const DISCOURAGE_INAPP_BROWSER_TEXT =
 
 export const HOW_TO0_TEXT = 'Comment Jouer'
 export const HOW_TO1_TEXT =
-  'Devinez le mot en 8 ou 9 essais. Après chaque essai, la couleur des tuiles changera pour montrer à quel point votre essai était proche du mot.'
-export const HOW_TO2_TEXT = 'La lettre B est dans le mot et au bon endroit.'
+  'Devinez la terme en 8 ou 9 essais. Après chaque essai, la couleur des tuiles changera pour montrer à quel point votre essai était proche de la terme.'
+export const HOW_TO2_TEXT = 'La lettre B est dans la terme et au bon endroit.'
 export const HOW_TO3_TEXT =
-  'La lettre I est dans le mot mais au mauvais endroit.'
-export const HOW_TO4_TEXT = "La nombre 7 n'est dans le mot à aucun endroit."
+  'La lettre I est dans la terme mais au mauvais endroit.'
+export const HOW_TO4_TEXT = "La nombre 7 n'est dans la terme à aucun endroit."
 
-export const HOW_TO5_TEXT = `Il s'agit d'un Wordle utilisant des mots liés à Omega Strikers et à sa scène compétitive : joueurs, tournois, mots d'argot, arenas, personnages, etc.`
+export const HOW_TO5_TEXT = `Il s'agit d'un Wordle utilisant des termes liés à Omega Strikers et à sa scène compétitive : joueurs, tournois, mots d'argot, arenas, personnages, etc.`
 export const HOW_TO6_TEXT = 'contactez-moi'
 export const HOW_TO7_TEXT = 'code source'
 export const HOW_TO8_TEXT = 'bifurqué de Reactle'
--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -5,7 +5,7 @@ export const GAME_URL = 'https://coredle.kuyachi.xyz'
 export const WIN_MESSAGES = ['Great Job!', 'Awesome', 'Well done!']
 export const GAME_COPIED_MESSAGE = 'Game copied to clipboard'
 export const NOT_ENOUGH_LETTERS_MESSAGE = 'Not enough letters'
-export const WORD_NOT_FOUND_MESSAGE = 'Word not found'
+export const WORD_NOT_FOUND_MESSAGE = 'Term not found'
 export const HARD_MODE_ALERT_MESSAGE =
   'Hard Mode can only be enabled at the start!'
 export const CORRECT_WORD_MESSAGE = (solution: string) =>
@@ -25,8 +25,8 @@ export const ENTER_TEXT = 'Enter'
 export const DELETE_TEXT = 'Delete'
 export const STATISTICS_TITLE = 'Statistics'
 export const GUESS_DISTRIBUTION_TEXT = 'Guess Distribution'
-export const NEW_WORD_TEXT = 'New word in'
-export const NEXT_WORD_TEXT = 'Next Word'
+export const NEW_WORD_TEXT = 'New term in'
+export const NEXT_WORD_TEXT = 'Next Term'
 export const SHARE_TEXT = 'Share'
 export const WITH_LINK_TEXT = 'With Link'
 export const TOTAL_TRIES_TEXT = 'Total tries'
@@ -44,14 +44,14 @@ export const DISCOURAGE_INAPP_BROWSER_TEXT =
 
 export const HOW_TO0_TEXT = 'How to Play'
 export const HOW_TO1_TEXT =
-  'Guess the word in 8-9 tries. After each guess, the color of the tiles will change to show how close your guess was to the word.'
+  'Guess the term in 8-9 tries. After each guess, the color of the tiles will change to show how close your guess was to the answer.'
 export const HOW_TO2_TEXT =
-  'The letter B is in this word and in the correct spot.'
+  'The letter B is in this term and in the correct spot.'
 export const HOW_TO3_TEXT =
-  'The letter I is in this word but in the wrong spot.'
-export const HOW_TO4_TEXT = 'The number 7 is not in this word in any spot.'
+  'The letter I is in this term but in the wrong spot.'
+export const HOW_TO4_TEXT = 'The number 7 is not in this term in any spot.'
 
-export const HOW_TO5_TEXT = `This is Wordle using words related to Omega Strikers and its competitive scene, ft. players, tournaments, slang, arenas, characters, you name it.`
+export const HOW_TO5_TEXT = `This is Wordle using terms related to Omega Strikers and its competitive scene, ft. players, tournaments, slang, arenas, characters, you name it.`
 export const HOW_TO6_TEXT = 'contact me'
 export const HOW_TO7_TEXT = 'source code'
 export const HOW_TO8_TEXT = 'forked from Reactle'
