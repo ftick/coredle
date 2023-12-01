3ebc6f97a7051f46ea76492897933769303b81fb
Toph Bantoto <lcbantoto@gmail.com>
2023-12-01 14:55:08 -0500

    change twitter link

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -110,7 +110,7 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
 
       <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
         <a
-          href="https://twitter.com/kuyachi_"
+          href="https://twitter.com/achichug"
           target="_blank"
           rel="noopener noreferrer"
           className="underline font-bold"
